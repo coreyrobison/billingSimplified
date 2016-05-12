241bdd2ec8e1143920c5d6ba3b1bbe96998c16c3
Corey Robison <coreybrobison@gmail.com>
2016-05-11 19:06:25 -0600

    new chat, changes to pmt view

---
+++ app/shared/chat/chatDirective.js
@@ -0,0 +1,28 @@
+angular.module('app').directive('chatDir', function () {
+
+    return {
+        restrict: 'E',
+        templateUrl: 'shared/chat/chatView.html',
+        link: function (scope, element, atts) {
+            (function () {
+
+                $('#live-chat header').on('click', function () {
+
+                    $('.chat').slideToggle(300, 'swing');
+                    $('.chat-message-counter').fadeToggle(300, 'swing');
+
+                });
+
+                $('.chat-close').on('click', function (e) {
+
+                    e.preventDefault();
+                    $('#live-chat').fadeOut(300);
+
+                });
+
+            })();
+        }
+    };
+});
+
+
---
+++ app/shared/graphs/graphsDirective.js
@@ -0,0 +1,8 @@
+angular.module('app').directive('graphsDir', function () {
+
+    return {
+        restrict: 'E',
+        templateUrl: 'shared/graphs/graphsView.html'
+    };
+});
+        
