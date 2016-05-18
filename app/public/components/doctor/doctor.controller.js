69a6f67fc77735aed4c7748e8e8c2d3438ca7edc
Corey Robison <coreybrobison@gmail.com>
2016-05-18 16:11:34 -0600

    modal pop

--- app/public/components/doctor/doctor.controller.js
+++ app/public/components/doctor/doctor.controller.js
@@ -1,13 +1,13 @@
 angular.module("app").controller('doctorCtrl', function ($scope, $state, doctorService, patientService) {
 
-    $scope.getPatients = function() {
-        patientService.getPatient().then(function(response) {
+    $scope.getPatients = function () {
+        patientService.getPatient().then(function (response) {
             $scope.patients = response;
             console.log(response);
         })
     }
     $scope.getPatients();
-    
+
     //When user clicks submit will add new patient to collection
     $scope.addPatient = function (newPatient) {
         patientService.addPatient(newPatient).then(function (response) {
@@ -15,13 +15,12 @@ angular.module("app").controller('doctorCtrl', function ($scope, $state, doctorS
             $scope.getPatients();
         });
         $state.reload('patient');
-        $modalInstance.close();
+        $scope.patient = !$scope.patient;
     };
-    
+
     //User can cancel adding a new patient
     $scope.cancel = function () {
         $scope.patient = !$scope.patient;
-        $modalInstance.dismiss('cancel');
     };
 
 });
