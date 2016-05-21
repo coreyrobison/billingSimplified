50bc700ba3827e586f2d4cc2674fdb3af71110ff
Corey Robison <coreybrobison@gmail.com>
2016-05-21 16:33:00 -0600

    the filter works now!!!

--- app/public/app.filter.js
+++ app/public/app.filter.js
@@ -1,9 +1,5 @@
-angular.module('app').filter('patientFilter', function($scope){
-  var filtered = []
-  return function(selectedOption){
-    if(selectedOption === $scope.patients.name && $scope.payments.patient === $scope.patient._id){
-      filtered.push($scope.patient.payments)
+angular.module('app').filter('yesNo', function() {
+    return function(input) {
+        return input ? 'YES' : 'NO';
     }
-    return filtered
-  }
-});
+})
